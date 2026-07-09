74dd7853b177b845111a60883e12d18264b2619a
darling <tiffany.nayeon20@gmail.com>
2026-07-09 10:51:37 -0500

    Corregir tamaño en Android

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.floatlayout import FloatLayout
 
 from screens.calculadora import CalculadoraScreen
 from screens.convertidor import ConvertidorScreen
@@ -11,12 +12,16 @@ class CalculadoraProApp(App):
     def build(self):
         Builder.load_file("calculadora.kv")
 
-        sm = ScreenManager()
+        root = FloatLayout()
+
+        sm = ScreenManager(size_hint=(1, 1), pos_hint={"x": 0, "y": 0})
         sm.add_widget(CalculadoraScreen(name="calculadora"))
         sm.add_widget(ConvertidorScreen(name="convertidor"))
         sm.add_widget(HistorialScreen(name="historial"))
 
-        return sm
+        root.add_widget(sm)
+
+        return root
 
 
 if __name__ == "__main__":
